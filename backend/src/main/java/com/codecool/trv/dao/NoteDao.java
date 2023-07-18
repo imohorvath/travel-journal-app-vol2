4ea313bb2b471a2e2c19734ead1fba2c2a9638f3
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-07-18 19:31:35 +0200

    implement findAllNotesByJournalId method

--- backend/src/main/java/com/codecool/trv/dao/NoteDao.java
+++ backend/src/main/java/com/codecool/trv/dao/NoteDao.java
@@ -3,6 +3,7 @@ package com.codecool.trv.dao;
 import com.codecool.trv.dto.note.Note;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class NoteDao {
@@ -13,10 +14,14 @@ public class NoteDao {
         this.notes = new ArrayList<>();
     }
 
-    public List<Note> findAllNotesInJournalById(int id) {
-        //return notes.stream().filter(note -> note.getJournal().getId() == id).toList();
-        return notes;
+    public List<Note> findAllNotesByJournalId(int id) {
+        return notes
+                .stream()
+                .filter(note -> note.getJournal().getId() == id)
+                .sorted(Comparator.comparing(Note::getTimestamp))
+                .toList();
     }
+
 
 
 }
