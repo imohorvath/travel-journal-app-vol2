611de59695d7e5ff9b87e011451e2571727231e9
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-07-18 14:26:28 +0200

    create journal getAll endpoint

--- backend/src/main/java/com/codecool/trv/controller/JournalController.java
+++ backend/src/main/java/com/codecool/trv/controller/JournalController.java
@@ -1,9 +1,44 @@
 package com.codecool.trv.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codecool.trv.dto.Journal;
+import com.codecool.trv.dto.user.NewUser;
+import com.codecool.trv.dto.user.User;
+import com.codecool.trv.service.JournalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("journal")
 public class JournalController {
+
+    private final JournalService journalService;
+
+    @Autowired
+    public JournalController(JournalService journalService) {
+        this.journalService = journalService;
+    }
+
+    @GetMapping("/all")
+    public List<Journal> findAllJournals() {
+        return journalService.findAllJournals();
+    }
+
+//    @GetMapping("/{id}")
+//    public User findUserById(@PathVariable int id) {
+//        return journalService.findUserById(id);
+//    }
+//
+//    @PostMapping("/")
+//    public User addNewUser(@RequestBody NewUser newUser) {
+//        return journalService.addNewUser(newUser);
+//    }
+//
+//    @DeleteMapping("/")
+//    public List<User> deleteAllUsers() {
+//        return journalService.deleteAllUsers();
+//    }
+//
+
 }
