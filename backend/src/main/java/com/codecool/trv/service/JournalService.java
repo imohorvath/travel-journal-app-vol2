f1402eb66bee0fc7117fd41e74a30fa43c07be36
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-07-18 16:41:50 +0200

    implement findAlljournalsByUserId and addNewJournal methods to service class

--- backend/src/main/java/com/codecool/trv/service/JournalService.java
+++ backend/src/main/java/com/codecool/trv/service/JournalService.java
@@ -1,7 +1,9 @@
 package com.codecool.trv.service;
 
 import com.codecool.trv.dao.JournalDao;
-import com.codecool.trv.dto.Journal;
+import com.codecool.trv.dao.UserDao;
+import com.codecool.trv.dto.journal.Journal;
+import com.codecool.trv.dto.journal.NewJournal;
 import com.codecool.trv.dto.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,14 +13,25 @@ import java.util.List;
 @Service
 public class JournalService {
 
-    private JournalDao journalDao;
-
+    private final JournalDao journalDao;
+    private final UserDao userDao;
     @Autowired
-    public JournalService(JournalDao journalDao) {
+    public JournalService(JournalDao journalDao, UserDao userDao) {
         this.journalDao = journalDao;
+        this.userDao = userDao;
     }
 
-    public List<Journal> findAllJournals() {
-        return journalDao.findAllJournals();
+    public List<Journal> findAllJournalsByUserId(int userId) {
+        return journalDao.findAllJournalsByUserId(userId);
+    }
+
+    public Journal findJournalById(int id) {
+        return journalDao.findJournalById(id);
+    }
+
+    public Journal addNewJournal(NewJournal newJournal) {
+        User user = userDao.findUserById(newJournal.getOwnerId());
+        int id = journalDao.addNewJournal(newJournal, user);
+        return findJournalById(id);
     }
 }
