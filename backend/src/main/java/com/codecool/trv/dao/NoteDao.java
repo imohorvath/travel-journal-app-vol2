e75e81c2d87b4320493586f0ed971f5ac9fa7c25
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-07-18 17:04:16 +0200

    implement NoteDao class

--- backend/src/main/java/com/codecool/trv/dao/NoteDao.java
+++ backend/src/main/java/com/codecool/trv/dao/NoteDao.java
@@ -1,4 +1,22 @@
 package com.codecool.trv.dao;
 
+import com.codecool.trv.dto.note.Note;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class NoteDao {
+
+    private final List<Note> notes;
+
+    public NoteDao() {
+        this.notes = new ArrayList<>();
+    }
+
+    public List<Note> findAllNotesInJournalById(int id) {
+        //return notes.stream().filter(note -> note.getJournal().getId() == id).toList();
+        return notes;
+    }
+
+
 }
