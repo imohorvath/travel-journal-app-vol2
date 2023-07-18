586926d3add43008c24636e2e9a2d260a573a0c9
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-07-18 17:04:29 +0200

    implement NoteService class

--- backend/src/main/java/com/codecool/trv/service/NoteService.java
+++ backend/src/main/java/com/codecool/trv/service/NoteService.java
@@ -1,7 +1,32 @@
 package com.codecool.trv.service;
 
+import com.codecool.trv.dao.JournalDao;
+import com.codecool.trv.dao.NoteDao;
+import com.codecool.trv.dao.UserDao;
+import com.codecool.trv.dto.note.Note;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class NoteService {
+
+    private final NoteDao noteDao;
+    private final JournalDao journalDao;
+    private final UserDao userDao;
+
+    public NoteService(NoteDao noteDao, JournalDao journalDao, UserDao userDao) {
+        this.noteDao = noteDao;
+        this.journalDao = journalDao;
+        this.userDao = userDao;
+    }
+
+    public List<Note> findAllNotesInJournalById(int id) {
+        return noteDao.findAllNotesInJournalById(id);
+    }
+
+
+    public Note addNewNoteToJournal() {
+        return null;
+    }
 }
