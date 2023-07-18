69fe61b6d8bc4a5fc9e332c78e1d55daeb3cbfea
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-07-18 20:08:43 +0200

    refactor Note class

--- backend/src/main/java/com/codecool/trv/dto/note/Note.java
+++ backend/src/main/java/com/codecool/trv/dto/note/Note.java
@@ -9,31 +9,31 @@ import java.time.LocalDateTime;
 public class Note {
 
     private final int id;
-    private final String note;
+    private final String text;
     private final LocalDateTime timestamp;
     private final GeoIP geoIP;
     private final Journal journal;
-    private final User user;
+    private final User creator;
 
-    public Note(int id, String note, LocalDateTime timestamp, GeoIP geoIP, Journal journal, User user) {
+    public Note(int id, String text, GeoIP geoIP, Journal journal, User creator) {
         this.id = id;
-        this.note = note;
-        this.timestamp = timestamp;
+        this.text = text;
+        this.timestamp = LocalDateTime.now();
         this.geoIP = geoIP;
         this.journal = journal;
-        this.user = user;
+        this.creator = creator;
     }
 
     public int getId() {
         return id;
     }
 
-    public String getNote() {
-        return note;
+    public String getText() {
+        return text;
     }
 
-    public LocalDateTime getTimestamp() {
-        return timestamp;
+    public String getTimestamp() {
+        return timestamp.toString();
     }
 
     public GeoIP getGeoIP() {
@@ -44,7 +44,7 @@ public class Note {
         return journal;
     }
 
-    public User getUser() {
-        return user;
+    public User getCreator() {
+        return creator;
     }
 }
