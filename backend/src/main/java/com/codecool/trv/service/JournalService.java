981fb87348a7256461daccdd4e8f4412070626ea
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-07-18 14:27:28 +0200

    implement JournalService class with findAllJournals method

--- backend/src/main/java/com/codecool/trv/service/JournalService.java
+++ backend/src/main/java/com/codecool/trv/service/JournalService.java
@@ -1,7 +1,24 @@
 package com.codecool.trv.service;
 
+import com.codecool.trv.dao.JournalDao;
+import com.codecool.trv.dto.Journal;
+import com.codecool.trv.dto.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class JournalService {
+
+    private JournalDao journalDao;
+
+    @Autowired
+    public JournalService(JournalDao journalDao) {
+        this.journalDao = journalDao;
+    }
+
+    public List<Journal> findAllJournals() {
+        return journalDao.findAllJournals();
+    }
 }
