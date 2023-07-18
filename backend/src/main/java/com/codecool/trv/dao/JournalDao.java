4d3025887cd7665ee3bf55589f8144eae522f7e7
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-07-18 14:27:12 +0200

    implement JournalDao class with findAllJournals method

--- backend/src/main/java/com/codecool/trv/dao/JournalDao.java
+++ backend/src/main/java/com/codecool/trv/dao/JournalDao.java
@@ -1,6 +1,24 @@
 package com.codecool.trv.dao;
 
 
+import com.codecool.trv.dto.Journal;
+import com.codecool.trv.dto.user.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JournalDao {
 
+    private final List<Journal> journals;
+
+    private static int idCounter = 0;
+
+    public JournalDao() {
+        this.journals = new ArrayList<>();
+    }
+
+
+    public List<Journal> findAllJournals() {
+        return journals;
+    }
 }
