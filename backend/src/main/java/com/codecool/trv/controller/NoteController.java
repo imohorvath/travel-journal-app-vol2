a5514c40b5ea874eec08361e12029b41fdfd7a3f
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-07-18 17:04:41 +0200

    implement NoteController class

--- backend/src/main/java/com/codecool/trv/controller/NoteController.java
+++ backend/src/main/java/com/codecool/trv/controller/NoteController.java
@@ -1,9 +1,42 @@
 package com.codecool.trv.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codecool.trv.dto.note.NewNote;
+import com.codecool.trv.dto.note.Note;
+import com.codecool.trv.service.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("note")
 public class NoteController {
+
+    private final NoteService noteService;
+
+    @Autowired
+    public NoteController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @GetMapping("/{journalId}/all")
+    public List<Note> findAllNotesInJournalById(@PathVariable int journalId) {
+        return noteService.findAllNotesInJournalById(journalId);
+    }
+
+    @PostMapping("/")
+    public Note addNewNoteToJournal(@RequestBody NewNote newNote) {
+        return noteService.addNewNoteToJournal();
+    }
+
+    @PutMapping("/")
+    public Note updateNoteById() {
+        return null;
+    }
+
+    @DeleteMapping("")
+    public Note deleteNoteById() {
+        return null;
+    }
+
 }
