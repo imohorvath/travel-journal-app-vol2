8a94c3d2ce23a3135921818ef78673f85779378a
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-07-18 16:40:35 +0200

    implement findAlljournalsByUerId and findJournalById methods

--- backend/src/main/java/com/codecool/trv/dao/JournalDao.java
+++ backend/src/main/java/com/codecool/trv/dao/JournalDao.java
@@ -1,11 +1,13 @@
 package com.codecool.trv.dao;
 
 
-import com.codecool.trv.dto.Journal;
+import com.codecool.trv.dto.journal.Journal;
+import com.codecool.trv.dto.journal.NewJournal;
 import com.codecool.trv.dto.user.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class JournalDao {
 
@@ -17,8 +19,26 @@ public class JournalDao {
         this.journals = new ArrayList<>();
     }
 
+    public List<Journal> findAllJournalsByUserId(int userId) {
+        // todo search between contributors as well
+       return journals.stream().filter(journal -> journal.getOwner().getId() == userId).toList();
+    }
 
-    public List<Journal> findAllJournals() {
-        return journals;
+    public Journal findJournalById(int id) {
+        return journals.stream()
+                .filter(journal -> journal.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new NoSuchElementException("No journal found."));
+    }
+
+    public int addNewJournal(NewJournal newJournal, User user) {
+        idCounter++;
+        Journal journal = new Journal(
+                idCounter,
+                newJournal.getTitle(),
+                user
+        );
+        journals.add(journal);
+        return idCounter;
     }
 }
