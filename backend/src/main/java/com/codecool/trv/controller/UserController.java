4e9779729c9185fe5c89d113d97ff0ace4ac2fef
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-07-18 13:08:19 +0200

    implement UserController class

--- backend/src/main/java/com/codecool/trv/controller/UserController.java
+++ backend/src/main/java/com/codecool/trv/controller/UserController.java
@@ -1,9 +1,42 @@
 package com.codecool.trv.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codecool.trv.dto.user.NewUser;
+import com.codecool.trv.dto.user.User;
+import com.codecool.trv.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/all")
+    public List<User> findAllUsers() {
+        return userService.findAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User findUserById(@PathVariable int userId) {
+        return userService.findUserById(userId);
+    }
+
+    @PostMapping("/")
+    public User addNewUser(@RequestBody NewUser newUser) {
+        return userService.addNewUser(newUser);
+    }
+
+    @DeleteMapping("/")
+    public List<User> deleteAllUsers() {
+        return userService.deleteAllUsers();
+    }
+
 }
