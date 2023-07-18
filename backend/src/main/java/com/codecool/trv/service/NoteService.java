8086d766f6f4941508a9cfceaf793ecd00b78fb2
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-07-18 20:07:35 +0200

    implement addNewNoteToJournal method

--- backend/src/main/java/com/codecool/trv/service/NoteService.java
+++ backend/src/main/java/com/codecool/trv/service/NoteService.java
@@ -3,7 +3,10 @@ package com.codecool.trv.service;
 import com.codecool.trv.dao.JournalDao;
 import com.codecool.trv.dao.NoteDao;
 import com.codecool.trv.dao.UserDao;
+import com.codecool.trv.dto.journal.Journal;
+import com.codecool.trv.dto.note.NewNote;
 import com.codecool.trv.dto.note.Note;
+import com.codecool.trv.dto.user.User;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -25,8 +28,9 @@ public class NoteService {
         return noteDao.findAllNotesByJournalId(id);
     }
 
-
-    public Note addNewNoteToJournal() {
-        return null;
+    public Note addNewNoteToJournal(NewNote newNote) {
+        User creator = userDao.findUserById(newNote.getUserId());
+        Journal journal = journalDao.findJournalById(newNote.getJournalId());
+        return noteDao.addNewNoteToJournal(newNote.getText(), journal, creator);
     }
 }
