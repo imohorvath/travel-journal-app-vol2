c2dbd1176b47b16911b757963f3d5d789699e282
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-07-18 17:03:44 +0200

    create GeoIP class with base data fields

---
+++ backend/src/main/java/com/codecool/trv/dto/geoip/GeoIP.java
@@ -0,0 +1,12 @@
+package com.codecool.trv.dto.geoip;
+
+public class GeoIP {
+
+    private String ipAddress;
+    private String device;
+    private String city;
+    private String fullLocation;
+    private Double latitude;
+    private Double longitude;
+
+}
