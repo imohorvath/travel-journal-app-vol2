a231ea48d095933105d2ccc8ef427ae6bfa374ac
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-07-19 11:08:45 +0200

    fetch journals

--- frontend/src/Components/Journals/Journals.jsx
+++ frontend/src/Components/Journals/Journals.jsx
@@ -1,6 +1,25 @@
 import "./Journals.css";
+import {useEffect, useState} from "react";
 
 const Journals = () => {
+    const [journalList, setJournalList] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch("/api/journal")
+            .then((res) => res.json())
+            .then((result) => {
+                setJournalList(result);
+                setLoading(false);
+            })
+            .catch((error) =>
+                console.log(`An error occurred at fetching from /api/journal:${error}`)
+            );
+    }, [])
+
+    // if (loading) {
+    //     return <Loading />;
+    // }
 
     return (
         <div>
