cd2d9e94167ede92c40f2e4e8b688bb60446fc84
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-07-19 13:07:09 +0200

    add Journal component to Main

--- frontend/src/Pages/Main/Main.jsx
+++ frontend/src/Pages/Main/Main.jsx
@@ -1,11 +1,15 @@
 import "./Main.css";
+import Journals from "../../Components/Journals";
 
 const Main = () => {
 
     return (
+        <>
         <div>
             <h1>Hello Main!</h1>
         </div>
+            <Journals />
+        </>
     );
 
 }
