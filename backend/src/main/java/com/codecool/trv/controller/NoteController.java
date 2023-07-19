c3b0c546b06fd71b8c34c0f496e36bd3c5a98fa4
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-07-19 11:09:56 +0200

    extend endpoint paths with 'api/' expression

--- backend/src/main/java/com/codecool/trv/controller/NoteController.java
+++ backend/src/main/java/com/codecool/trv/controller/NoteController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("note")
+@RequestMapping("api/note")
 public class NoteController {
 
     private final NoteService noteService;
