cfab75c2edc2a66317888a6d3f7bff722d8139f4
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-07-20 14:16:23 +0200

    fix popover menu and implement handleClose method

--- frontend/src/Pages/Header/Header.jsx
+++ frontend/src/Pages/Header/Header.jsx
@@ -1,5 +1,5 @@
 import "./Header.css";
-import {Outlet} from "react-router-dom";
+import {Outlet, useNavigate} from "react-router-dom";
 import Button from "@mui/material/Button";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
@@ -12,30 +12,45 @@ import {IconButton, Menu, MenuItem, Tooltip} from "@mui/material";
 
 const Header = () => {
 
-    const [auth, setAuth] = useState(false);
-    const [openMenu, setOpenMenu] = useState(false);
+    const [auth, setAuth] = useState(true);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+
+    const navigate = useNavigate();
 
     const settings = ['Profile', 'Journals', 'Logout'];
 
-    const handleOpenUserMenu = (event) => {
-        console.log(event);
-        setOpenMenu(true);
-        //setAnchorElUser(event.currentTarget);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
     };
-
-    const handleCloseUserMenu = (event) => {
-        console.log(event);
-        setOpenMenu(false);
-        //setAnchorElUser(null);
+    const handleClose = (event) => {
+        switch (event.target.innerText) {
+            case "Profile" : {
+                navigate(`/profile`);
+                break;
+            }
+            case "Journals" : {
+                navigate(`/main`);
+                break;
+            }
+            case "Logout" : {
+                navigate(`/`);
+                setAuth(false);
+                break;
+            }
+        }
+        setAnchorEl(null);
     };
 
     return (
         <>
             <AppBar
                 position="static"
-                color="default"
                 elevation={0}
-                sx={{borderBottom: (theme) => `1px solid ${theme.palette.divider}`}}
+                sx={{
+                    borderBottom: 1,
+                    bgcolor: "#d6a27c"
+                }}
             >
                 <Toolbar sx={{flexWrap: 'wrap'}}>
                     <Typography variant="h5" color="inherit" noWrap sx={{flexGrow: 1}}>
@@ -44,15 +59,19 @@ const Header = () => {
                     {!auth && <Button href="#" variant="outlined" sx={{my: 1, mx: 1.5}}>
                         Login
                     </Button>}
-                    {!auth && <Box sx={{flexGrow: 0}}>
+                    {auth && <Box sx={{flexGrow: 0}}>
                         <Tooltip title="Open settings">
-                            <IconButton onClick={(event)=>handleOpenUserMenu(event)} sx={{p: 0}}>
+                            <IconButton
+                                onClick={handleClick}
+                                sx={{p: 0}}
+                            >
                                 <Avatar alt="Pinguin" src="../../img/animal(6).png"/>
                             </IconButton>
                         </Tooltip>
-                        {openMenu && <Menu
+                        <Menu
                             sx={{mt: '45px'}}
                             id="menu-appbar"
+                            anchorEl={anchorEl}
                             anchorOrigin={{
                                 vertical: 'top',
                                 horizontal: 'right',
@@ -62,14 +81,15 @@ const Header = () => {
                                 vertical: 'top',
                                 horizontal: 'right',
                             }}
-                            onClose={(event)=>handleCloseUserMenu(event)}
+                            open={open}
+                            onClose={handleClose}
                         >
                             {settings.map((setting) => (
-                                <MenuItem key={setting} onClick={(event)=>handleCloseUserMenu(event)}>
+                                <MenuItem key={setting} onClick={handleClose}>
                                     <Typography textAlign="center">{setting}</Typography>
                                 </MenuItem>
                             ))}
-                        </Menu>}
+                        </Menu>
                     </Box>}
                 </Toolbar>
             </AppBar>
