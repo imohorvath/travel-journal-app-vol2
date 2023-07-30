178e5b12ddcd16c368dc8bb656516b1afb760741
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-07-30 23:15:22 +0200

    refactor findAllUsers method

--- backend/src/main/java/com/codecool/trv/controller/UserController.java
+++ backend/src/main/java/com/codecool/trv/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.codecool.trv.controller;
 
 import com.codecool.trv.dto.user.NewUserRequest;
+import com.codecool.trv.dto.user.UserResponse;
 import com.codecool.trv.model.User;
 import com.codecool.trv.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,21 +20,25 @@ public class UserController {
         this.userService = userService;
     }
 
+    //FIXME: This might be unnecessary, or admin privilage --- or return UserResponse instead at least
     @GetMapping("/")
-    public List<User> findAllUsers() {
+    public List<UserResponse> findAllUsers() {
         return userService.findAllUsers();
     }
 
+    //FIXME: This might be unnecessary, in userservice is enough to have a findUserById() --- or return UserResponse instead
     @GetMapping("/{id}")
-    public User findUserById(@PathVariable Long id) {
+    public UserResponse findUserById(@PathVariable Long id) {
         return userService.findUserById(id);
     }
+
 
     @PostMapping("/")
     public User addNewUser(@RequestBody NewUserRequest newUserRequest) {
         return userService.addNewUser(newUserRequest);
     }
 
+    //FIXME: This is only for testing purposes
     @DeleteMapping("/")
     public List<User> deleteAllUsers() {
         return userService.deleteAllUsers();
