3cc2a4f6f9b2e631d423a906561164fb16560fd1
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-07-30 20:23:12 +0200

    add @Repository annotation

--- backend/src/main/java/com/codecool/trv/repository/JournalRepository.java
+++ backend/src/main/java/com/codecool/trv/repository/JournalRepository.java
@@ -2,6 +2,8 @@ package com.codecool.trv.repository;
 
 import com.codecool.trv.model.Journal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JournalRepository extends JpaRepository<Journal, Long> {
 }
--- backend/src/main/java/com/codecool/trv/repository/NoteRepository.java
+++ backend/src/main/java/com/codecool/trv/repository/NoteRepository.java
@@ -3,6 +3,8 @@ package com.codecool.trv.repository;
 import com.codecool.trv.model.Journal;
 import com.codecool.trv.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
 }
