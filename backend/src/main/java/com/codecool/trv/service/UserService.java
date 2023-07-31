f68a481e98d6026fb1d4d6b51f8269c0eb885c48
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-07-31 06:59:36 +0200

    implement addUser method leave TODO comment for validation

--- backend/src/main/java/com/codecool/trv/service/UserService.java
+++ backend/src/main/java/com/codecool/trv/service/UserService.java
@@ -1,6 +1,7 @@
 package com.codecool.trv.service;
 
 import com.codecool.trv.dto.user.NewUserRequest;
+import com.codecool.trv.dto.user.UpdateUserRequest;
 import com.codecool.trv.dto.user.UserResponse;
 import com.codecool.trv.exception.ResourceNotFoundException;
 import com.codecool.trv.model.User;
@@ -25,13 +26,13 @@ public class UserService {
         return users.stream().map(user -> new UserResponse(user.getId(), user.getUsername())).toList();
     }
 
-    public UserResponse findUserById(Long userId) {
-        User user = userRepository.findById(userId).orElseThrow(()->new ResourceNotFoundException("User not found with id: " + userId));
+    public UserResponse findUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("User not found with id: " + id));
         return new UserResponse(user.getId(), user.getUsername());
     }
 
-    public UserResponse addNewUser(NewUserRequest newUserRequest) {
-        //TODO validation!!! - unique username, unique email
+    public UserResponse addUser(NewUserRequest newUserRequest) {
+        //TODO request validation!!! - unique username, unique email
         User userToSave = User
                 .builder()
                 .username(newUserRequest.username())
@@ -45,9 +46,38 @@ public class UserService {
         return new UserResponse(user.getId(), user.getUsername());
     }
 
+    public UserResponse updateUserById(Long id, UpdateUserRequest updateUserRequest) {
+        //TODO request validation!!! - unique username, unique email
+        //https://reflectoring.io/bean-validation-with-spring-boot/#using-validation-groups-to-validate-objects-differently-for-different-use-cases
+
+        /*Is's better because getOne(ID id) gets you only a reference (proxy) object
+         and does not fetch it from the DB. On this reference you can set what you want
+         and on save() it will do just an SQL UPDATE statement like you expect it.---maybe not :(
+         */
+        /*User userToUpdate;
+        try {
+            userToUpdate = userRepository.getOne(id);
+        } catch (Exception exception) {
+            throw new ResourceNotFoundException("User not found with id: " + id);
+        }*/
+
+        User userToUpdate = userRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("User not found with id: " + id));
+
+        userToUpdate.setUsername(updateUserRequest.username());
+        userToUpdate.setFirstName(updateUserRequest.firstName());
+        userToUpdate.setLastName(updateUserRequest.lastName());
+        userToUpdate.setEmail(updateUserRequest.email());
+        userToUpdate.setPassword(updateUserRequest.password());
+
+        userRepository.save(userToUpdate);
+        return new UserResponse(userToUpdate.getId(), userToUpdate.getUsername());
+    }
+
     public List<User> deleteAllUsers() {
         //TODO
         return null;
         //return userDao.deleteAllUsers();
     }
+
+
 }
