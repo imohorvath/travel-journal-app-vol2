2acf72da8efa15d8012064162494bd9fc3bb6584
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-08-01 13:04:46 +0200

    add findAllByJournal_Id method

--- backend/src/main/java/com/codecool/trv/repository/NoteRepository.java
+++ backend/src/main/java/com/codecool/trv/repository/NoteRepository.java
@@ -5,6 +5,11 @@ import com.codecool.trv.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
+
+    List<Note> findAllByJournal_Id(Long journalId);
+
 }
