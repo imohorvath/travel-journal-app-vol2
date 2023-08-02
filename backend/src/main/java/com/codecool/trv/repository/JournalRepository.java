46f1a16e0a3d4c3b4511704026cbedd15d0e7423
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-08-03 00:57:27 +0200

    remove unnecessary imports

--- backend/src/main/java/com/codecool/trv/repository/JournalRepository.java
+++ backend/src/main/java/com/codecool/trv/repository/JournalRepository.java
@@ -1,12 +1,10 @@
 package com.codecool.trv.repository;
 
 import com.codecool.trv.model.Journal;
-import com.codecool.trv.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Set;
 
 @Repository
 public interface JournalRepository extends JpaRepository<Journal, Long> {
--- backend/src/main/java/com/codecool/trv/repository/NoteRepository.java
+++ backend/src/main/java/com/codecool/trv/repository/NoteRepository.java
@@ -1,6 +1,5 @@
 package com.codecool.trv.repository;
 
-import com.codecool.trv.model.Journal;
 import com.codecool.trv.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
