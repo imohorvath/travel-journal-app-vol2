b40f6fdded34c73397bd732eea5e8ca1157a0b17
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-08-02 19:04:07 +0200

    implement handleDataIntegrityViolation method

--- backend/src/main/java/com/codecool/trv/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/codecool/trv/exception/GlobalExceptionHandler.java
@@ -1,27 +1,28 @@
 package com.codecool.trv.exception;
 
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    //TODO implement custom ApiError object in order to implement proper error handling towards the client
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> handleElementNotFound(ResourceNotFoundException exception) {
-        //ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage());
-        //return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(EmptyResultDataAccessException.class)
     public ResponseEntity<?> handleRecordNotFound(EmptyResultDataAccessException exception) {
-        //RestApiError error = new RestApiError(HttpStatus.BAD_REQUEST, Map.of("message", "Record not found"), request.getRequestURI());
-        //return new ResponseEntity<>(error, error.getHttpStatus());
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
     }
 
@@ -30,5 +31,12 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<Object> handleDataIntegrityViolation(DataIntegrityViolationException exception) {
+        if (exception.getCause() instanceof ConstraintViolationException) {
+            return new ResponseEntity<>("Database conflict", HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity<>("Server issue", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 }
