ed2df47dda744ceacb6a9fe55c3f1b6992e266c1
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-08-03 14:45:57 +0200

    change field type by User and Journal

--- backend/src/main/java/com/codecool/trv/dto/note/NoteResponse.java
+++ backend/src/main/java/com/codecool/trv/dto/note/NoteResponse.java
@@ -1,12 +1,13 @@
 package com.codecool.trv.dto.note;
 
+import com.codecool.trv.dto.user.UserResponse;
 import com.codecool.trv.model.GeoIP;
-import com.codecool.trv.model.Journal;
-import com.codecool.trv.model.User;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
 
 @Builder
-public record NoteResponse(Long id, String text, LocalDateTime createdAt, User createdBy, GeoIP geoIP, Journal journal, LocalDateTime updatedAt, User updatedBy) {
+public record NoteResponse(Long id, String text, LocalDateTime createdAt, UserResponse createdBy, GeoIP geoIP, Long journalId, LocalDateTime updatedAt, UserResponse updatedBy) {
 }
+
+
