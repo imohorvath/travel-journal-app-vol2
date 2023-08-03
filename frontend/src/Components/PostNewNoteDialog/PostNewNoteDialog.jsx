0e57efe82e2cccca0bc3cb237756b26b53a9f741
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-08-03 17:02:15 +0200

    implement PostNewDialog comp to create new notes

---
+++ frontend/src/Components/PostNewNoteDialog/PostNewNoteDialog.jsx
@@ -0,0 +1,68 @@
+import "./PostNewNoteDialog.css";
+import { useEffect, useState } from "react";
+
+import {
+  Box,
+  Button,
+  CardActions,
+  Container,
+  Stack,
+  Typography,
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  TextField,
+} from "@mui/material";
+
+const PostNewNoteDialog = ({ open, onClose, onSubmit }) => {
+    const [text, setText] = useState("");
+
+    const handleSave = (e) => {
+        e.preventDefault();
+
+        const newNote = {
+            text: text
+        };
+
+        onSubmit(newNote);
+    };
+
+  return (
+    <>
+      <Dialog onClose={onClose} open={open} onSubmit={onSubmit} fullWidth>
+        <DialogTitle>Let's preserve your precious memories</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            id="outlined-multiline-flexible"
+            label="Multiline"
+            type="text"
+            multiline
+            fullWidth
+            rows={4}
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <Button size="medium" onClick={handleSave}>
+            SUBMIT
+          </Button>
+          <Button size="medium" onClick={onClose}>
+            CANCEL
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default PostNewNoteDialog;
---
+++ frontend/src/Components/PostNewNoteDialog/index.js
@@ -0,0 +1,3 @@
+import PostNewNoteDialog from "./PostNewNoteDialog";
+
+export default PostNewNoteDialog;
