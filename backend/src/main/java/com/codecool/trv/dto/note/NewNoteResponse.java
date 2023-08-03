e643ae32515a3798345a3ef83bd2bf62551ca71e
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-08-03 17:05:58 +0200

    change fields in NewNoteResponse record

--- backend/src/main/java/com/codecool/trv/dto/note/NewNoteResponse.java
+++ backend/src/main/java/com/codecool/trv/dto/note/NewNoteResponse.java
@@ -1,9 +1,13 @@
 package com.codecool.trv.dto.note;
 
+import com.codecool.trv.dto.user.UserResponse;
+import com.codecool.trv.model.GeoIP;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
 
 @Builder
-public record NewNoteResponse(Long id, String text, LocalDateTime createdAt, String createdBy, String journalTitle, LocalDateTime updatedAt, String updatedBy) {
+public record NewNoteResponse(Long id, String text, LocalDateTime createdAt, UserResponse createdBy, GeoIP geoIP, Long journalId, LocalDateTime updatedAt, UserResponse updatedBy) {
 }
+/*public record NewNoteResponse(Long id, String text, LocalDateTime createdAt, String createdBy, String journalTitle, LocalDateTime updatedAt, String updatedBy) {
+}*/
