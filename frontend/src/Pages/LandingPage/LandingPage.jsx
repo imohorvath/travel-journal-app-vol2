01efe38a96bfbb163a732627b549253db4888b22
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-08-03 14:34:00 +0200

    remove raised={true}

--- frontend/src/Pages/LandingPage/LandingPage.jsx
+++ frontend/src/Pages/LandingPage/LandingPage.jsx
@@ -7,104 +7,96 @@ import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const handleRedirection = () => {
+    navigate(`/login`);
+  };
 
-    const handleRedirection = () => {
-        navigate(`/login`);
-    }
-
-    return (
-        <>
-            <Card raised={true}
-                  sx={{borderRadius: '0'}}
-            >
-                <CardActionArea>
-                    <CardMedia
-                        component="video"
-                        image={"../../video/banner.mp4"}
-                        muted
-                        autoPlay
-                        loop
-                    />
-                </CardActionArea>
-            </Card>
-            <Container>
-                <Paper
-                    elevation={0}
-                    sx={{
-                        mx: {xs: 0.5, sm: 5},
-                        my: {xs: 3, sm: 5},
-                        /*typography: {
-                            fontSize: {xs: 10, sm: 10}
-                        }*/
-                    }}
-                >
-                    <Typography
-                        variant="h5"
-                        textAlign="center"
-                        sx={{
-                            px: {xs: 1, sm: 3},
-                            py: {xs: 2, sm: 3}
-                        }}
-                    >
-                        Welcome to our Travel Journal app - your passport to preserving and sharing your adventures!
-                    </Typography>
-                    <Typography
-                        textAlign="center"
-                        sx={{
-                            px: {xs: 1, sm: 3},
-                            py: {xs: 2, sm: 3},
-                            fontSize: 18
-                        }}
-                    >
-                        Seamlessly record your travel experiences, snap photos, and jot down thoughts on-the-go with our
-                        user-friendly interface.
-                        Relive the magic of your journeys and inspire others to wander with your beautifully crafted
-                        digital travel journals.
-                    </Typography>
-                </Paper>
-                <Paper
-                    elevation={0}
-                    sx={{
-                        mx: {xs: 0.5, sm: 5},
-                        my: {xs: 3, sm: 5},
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}
-                >
-                    <Typography
-                        variant="h4"
-                        textAlign="center"
-                        sx={{
-                            px: {xs: 1, sm: 3},
-                            py: {xs: 2, sm: 3},
-                            fontFamily: 'Satisfy'
-                        }}
-                    >
-                        Are you ready for your next adventure?
-                    </Typography>
-                    <Button
-                        variant="contained"
-                        sx={{
-                            width: 150
-                        }}
-                        onClick={handleRedirection}
-                    >
-                        <Typography>
-                            Sign in
-                        </Typography>
-                    </Button>
-                </Paper>
-            </Container>
-        </>
-    );
-
+  return (
+    <>
+      <Card sx={{ borderRadius: "0" }}>
+          <CardActionArea>
+            <CardMedia
+              component="video"
+              image={"../../video/banner.mp4"}
+              muted
+              autoPlay
+              loop
+            />
+          </CardActionArea>
+      </Card>
+      <Container>
+        <Paper
+          elevation={0}
+          sx={{
+            mx: { xs: 0.5, sm: 5 },
+            my: { xs: 3, sm: 5 },
+          }}
+        >
+          <Typography
+            variant="h5"
+            textAlign="center"
+            sx={{
+              px: { xs: 1, sm: 3 },
+              py: { xs: 2, sm: 3 },
+            }}
+          >
+            Welcome to our Travel Journal app - your passport to preserving and
+            sharing your adventures!
+          </Typography>
+          <Typography
+            textAlign="center"
+            sx={{
+              px: { xs: 1, sm: 3 },
+              py: { xs: 2, sm: 3 },
+              fontSize: 18,
+            }}
+          >
+            Seamlessly record your travel experiences, snap photos, and jot down
+            thoughts on-the-go with our user-friendly interface. Relive the
+            magic of your journeys and inspire others to wander with your
+            beautifully crafted digital travel journals.
+          </Typography>
+        </Paper>
+        <Paper
+          elevation={0}
+          sx={{
+            mx: { xs: 0.5, sm: 5 },
+            my: { xs: 3, sm: 5 },
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Typography
+            variant="h4"
+            textAlign="center"
+            sx={{
+              px: { xs: 1, sm: 3 },
+              py: { xs: 2, sm: 3 },
+              fontFamily: "Satisfy",
+            }}
+          >
+            Are you ready for your next adventure?
+          </Typography>
+          <Button
+            variant="contained"
+            sx={{
+              width: 150,
+            }}
+            onClick={handleRedirection}
+          >
+            <Typography>Sign in</Typography>
+          </Button>
+        </Paper>
+      </Container>
+    </>
+  );
 };
 
 export default LandingPage;
