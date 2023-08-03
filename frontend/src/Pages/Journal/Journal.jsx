e385c33964e8d420c0fc04d4a6f879f8873119cc
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-08-03 15:38:35 +0200

    fetch image fro the journal header

--- frontend/src/Pages/Journal/Journal.jsx
+++ frontend/src/Pages/Journal/Journal.jsx
@@ -1,55 +1,76 @@
 import NoteList from "../../Components/NoteList";
 import "./Journal.css";
 
-import {useEffect, useState} from "react";
-import {useParams} from "react-router";
-import {Container, Typography} from "@mui/material";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { CardMedia, Container, Typography } from "@mui/material";
+
+import { createClient } from "pexels";
 
 const Journal = () => {
-    const [journal, setJournal] = useState({});
-    //const [loading, setLoading] = useState(true);
-    const {journalId} = useParams();
+  const [journal, setJournal] = useState({});
+  const { journalId } = useParams();
+  const [imageURL, setImageURL] = useState(null);
 
-    useEffect(() => {
-        fetch(`/api/v1/journals/${journalId}`)
-            .then((res) => res.json())
-            .then((result) => {
-                console.log(result);
-                setJournal(result);
-             //  setLoading(false);
-            })
-            .catch((error) =>
-                console.log(`An error occurred at fetching from /api/journal:${error}`)
-            );
-    }, [journalId]);
-    return (
-        <>
-            <Container
-                sx={{
-                    backgroundImage: `url(${'https://images.pexels.com/photos/669986/pexels-photo-669986.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'})`,
-                    backgroundSize: 'cover',
-                    height: 100,
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                }}
-            >
-                <Typography
-                    component="h4"
-                    variant="h4"
-                    align="center"
-                    color="text.primary"
-                    gutterBottom
-                    sx={{
-                        fontFamily: 'Satisfy'
-                    }}
-                >
-                    {journal.title}
-                </Typography>
-            </Container>
-            <NoteList/>
-        </>
+  useEffect(() => {
+    fetch(`/api/v1/journals/${journalId}`)
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        setJournal(result);
+      })
+      .catch((error) =>
+        console.log(`An error occurred at fetching from /api/journal:${error}`)
+      );
+  }, [journalId]);
+
+  useEffect(() => {
+    getPexelImage(journal.title, journal.id);
+  }, [journal]);
+
+  const getPexelImage = (query, id) => {
+    const client = createClient(
+      "ObPMaZKwS1wvcaQJAHUkbW9dryeMn2WgxOyIJCGH7NwMo0tRFpgcZgvx"
     );
+
+    client.photos
+      .search({ query, per_page: 1 })
+      .then((photo) => {
+        setImageURL(photo.photos[0].src.landscape);
+      })
+      .catch((error) =>
+        console.log(`An error occurred at fetching from Pexel`)
+      );
+  };
+
+  return (
+    <>
+      <CardMedia
+        sx={{
+          backgroundImage: `url(${imageURL})`,
+          backgroundSize: "cover",
+          height: 100,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography
+          component="h4"
+          variant="h4"
+          align="center"
+          color="text.primary"
+          gutterBottom
+          sx={{
+            fontFamily: "Satisfy",
+          }}
+        >
+          {journal.title}
+        </Typography>
+      </CardMedia>
+      <NoteList />
+    </>
+  );
 };
 
 export default Journal;
