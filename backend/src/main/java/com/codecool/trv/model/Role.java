0fb2abd86d989fd79359a9d3e5b4af70cb64b230
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-08-14 13:01:53 +0200

    add role model class

---
+++ backend/src/main/java/com/codecool/trv/model/Role.java
@@ -0,0 +1,28 @@
+package com.codecool.trv.model;
+
+import com.codecool.trv.security.RoleType;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+}
