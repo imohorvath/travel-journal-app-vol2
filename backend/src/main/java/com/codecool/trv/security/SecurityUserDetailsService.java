e00642f89c3fb002ec008b3ca49066121c7cbe83
Noemi Varga <115186887+noemi-varga@users.noreply.github.com>
2023-08-14 13:06:03 +0200

    create SecurityUserDetailsService and override loadUserByUsername

---
+++ backend/src/main/java/com/codecool/trv/security/SecurityUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.codecool.trv.security;
+
+import com.codecool.trv.exception.ResourceNotFoundException;
+import com.codecool.trv.repository.UserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public SecurityUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository
+                .findUserByUsername(username)
+                .map(SecurityUser::new)
+                .orElseThrow(() -> new ResourceNotFoundException("Username not found: " + username));
+    }
+}
