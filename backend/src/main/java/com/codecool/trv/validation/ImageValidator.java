09d917d5e0b5adf6297148ebd2bf88fec510786f
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-08-15 14:58:32 +0200

    refactor ImageValidator

--- backend/src/main/java/com/codecool/trv/validation/ImageValidator.java
+++ backend/src/main/java/com/codecool/trv/validation/ImageValidator.java
@@ -1,24 +1,66 @@
 package com.codecool.trv.validation;
 
-import jdk.jfr.ContentType;
+import org.apache.commons.imaging.FormatCompliance;
+import org.apache.commons.imaging.ImageFormat;
+import org.apache.commons.imaging.ImageFormats;
+import org.apache.commons.imaging.Imaging;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.Set;
+
 
 @Component
 public class ImageValidator {
 
-    private static final List<String> IMAGE_TYPES = Arrays.asList("image/jpeg", "image/jpg", "image/png", "image/gif", "image/bmp");
+    private static final Set<String> ALLOWED_EXTENSIONS = Set.of("jpg", "jpeg", "png", "gif", "bmp");
 
+    /**
+     * Checks if the file is empty.
+     *
+     * @param file The MultipartFile to check.
+     * @return true if the file is empty or null, false otherwise.
+     */
     public boolean isEmpty(MultipartFile file) {
         return file == null || file.isEmpty();
     }
 
-    public boolean isImage(MultipartFile file) {
-        System.out.println(file.getContentType());
-        return IMAGE_TYPES.contains(file.getContentType());
+    public boolean isValidImage(MultipartFile file) {
+        return hasValidFormatByGuessing(file) && hasValidExtension(file);
+    }
+
+    /**
+     * Validates if the provided file is a genuine image.
+     *
+     * @param file The file to validate.
+     * @return true if the file is a valid image, false otherwise.
+     */
+    private boolean hasValidFormatByGuessing(MultipartFile file) {
+        try {
+            ImageFormat format = Imaging.guessFormat(file.getBytes());
+            return format != ImageFormats.UNKNOWN;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    /**
+     * Validates if the provided file's extension is one of the allowed extensions.
+     *
+     * @param file The file to validate.
+     * @return true if the file's extension is allowed, false otherwise.
+     */
+    private boolean hasValidExtension(MultipartFile file) {
+        String extension = getFileExtension(file.getOriginalFilename());
+        System.out.println(extension);
+        return ALLOWED_EXTENSIONS.contains(extension.toLowerCase());
+    }
+
+    private String getFileExtension(String filename) {
+        if (filename == null || filename.lastIndexOf(".") == -1) {
+            return "";
+        }
+        return filename.substring(filename.lastIndexOf(".") + 1);
     }
 
 }
