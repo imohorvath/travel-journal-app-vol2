1b7fe2bb7f96eac5bba0b73bf401348294b6b13b
Imola Horvath <115141423+imohorvath@users.noreply.github.com>
2023-08-16 05:17:44 +0200

    reformat code

--- backend/src/main/java/com/codecool/trv/security/model/Token.java
+++ backend/src/main/java/com/codecool/trv/security/model/Token.java
@@ -2,32 +2,30 @@ package com.codecool.trv.security.model;
 
 import com.codecool.trv.model.User;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-    @Data
-    @Builder
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Entity
-    public class Token {
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Token {
 
-        @Id
-        @GeneratedValue
-        public Long id;
+    @Id
+    @GeneratedValue
+    public Long id;
 
-        @Column(unique = true)
-        public String token;
+    @Column(unique = true)
+    public String token;
 
-        public String tokenType = "BEARER";
+    public String tokenType = "Bearer";
 
-        public boolean revoked;
+    public boolean revoked;
 
-        public boolean expired;
+    public boolean expired;
 
-        @ManyToOne(fetch = FetchType.LAZY)
-        @JoinColumn(name = "user_id")
-        public User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    public User user;
 }
